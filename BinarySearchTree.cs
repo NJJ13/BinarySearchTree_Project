adc3e658c34c7044a01fcc01dade617f08476260
Nick Johnson <johnsn0913@gmail.com>
2021-02-05 13:34:22 -0600

    added binarysearch tree class and node class

---
+++ BinarySearchTree.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class BinarySearchTree
+    {
+        public Node RootNode;
+        public Node[] ChildNodes = new Node[2];
+
+
+        public void AddNode()
+        {
+
+        }
+        public void SearchTree()
+        {
+
+        }
+    }
+}
---
+++ Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class Node
+    {
+        public int data;
+        public Node ChildNode1;
+        public Node ChildNode2;
+
+        public Node(int data)
+        {
+            this.data = data;
+        }
+    }
+}
